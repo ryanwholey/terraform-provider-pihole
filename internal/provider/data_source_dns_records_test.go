0adcd0916bf41d0cc8b9121682bdb2b466850fd8
Jonas Lilja <jonas@lilja.io>
2024-07-07 17:10:38 +0200

    Adds basic test for fetching by the data resource.

---
+++ internal/provider/data_source_dns_records_test.go
@@ -0,0 +1,33 @@
+package provider
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"testing"
+)
+
+func TestAccDomainsData(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: `
+					resource "pihole_dns_record" "record" {
+					  domain = "foo.com"
+					  ip     = "127.0.0.1"
+					}
+s
+					data "pihole_dns_records" "records" {
+					  depends_on = [pihole_dns_record.record]
+					}
+				`,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.pihole_dns_records.records", "records.#", "1"),
+
+					resource.TestCheckResourceAttr("data.pihole_dns_records.records", "records.0.domain", "foo.com"),
+					resource.TestCheckResourceAttr("data.pihole_dns_records.records", "records.0.ip", "127.0.0.1"),
+				),
+			},
+		},
+	})
+}
